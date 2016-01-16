edefa9becad47946d1b311b40c96264fff353a1f
Anton Shilov <vaccum121@gmail.com>
2016-01-16 22:41:08 +0700

    Fix mistake_prob_d graph method

--- graph.py
+++ graph.py
@@ -5,8 +5,8 @@ import сalculations as calc
 
 def mistake_prob_n(d11, d12, d21, d22, m11, m12, m21, m22, p1, p2, k):
     data = [[], []]
-    for i in range(1, 20):
-        n = i * 40
+    for i in range(5, 400):
+        n = i * 2
         data[0].append(n)
         data[1].append(
                 calc.get_classifier_fault(n, d11, d12, d21, d22, m11, m12, m21, m22, p1, p2, k)['mist_prob'])
@@ -47,16 +47,18 @@ def mistake_prob_m(n, d11, d12, d21, d22, m11, m12, m21, m22, p1, p2, k):
 def mistake_prob_d(n, d11, d12, d21, d22, m11, m12, m21, m22, p1, p2, k):
     data = [[], []]
     d21_ch = d21
-    for i in range(1, 100):
-        d21_ch += 0.2
-        data[0].append(d21_ch - m11)
-        data[1].append(
-                calc.get_classifier_fault(n, d11, d12, d21, d22, m11, m12, d21_ch, m22, p1, p2, k)['mist_prob'])
+    for i in range(1, 80):
+        d21_ch += 0.1
+        data[0].append(d21_ch - d11)
+        res = 0
+        for j in range(10):
+            res += calc.get_classifier_fault(n, d11, d12, d21_ch, d22, m11, m12, m21, m22, p1, p2, k)['mist_prob']
+        res /= 25
+        data[1].append(res)
     plt.plot(data[0], data[1], '-')
     print(data[0])
     print(data[1])
     plt.show()
-
 
 # mistake_prob_d(1000, 4, 5, 5, 6, 10, 12, 15, 17, 0.5, 0.5, 1000)
 # mistake_prob_p(1000, 4, 5, 5, 6, 10, 12, 15, 17, 1000, )
